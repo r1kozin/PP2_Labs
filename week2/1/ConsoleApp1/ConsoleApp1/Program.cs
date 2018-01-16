5e224aadf6094eef25c3ca2fe2e252fc615d06a0
Rus <rustam121099@gmail.com>
2018-01-16 11:06:28 +0600

    lab 1

---
+++ week2/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { static bool isPrime (int n)
+        {
+            for (int i = 1; i < Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < args.Length; i++)
+                if (isPrime(int.Parse(args[i]))==true)
+                    Console.WriteLine(args[i]);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
